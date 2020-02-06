14933bc07f169b137555b3b6d716b063f89a03d5
Jared Moore <jmoore1127@gmail.com>
2020-02-05 19:51:42 -0700

    remove Zeldog prefix

--- precache-manifest.59c3e5136fdd40fae87ad3053d1c3751.js
+++ precache-manifest.59c3e5136fdd40fae87ad3053d1c3751.js
@@ -1,82 +1,82 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "21b79b671dbb2a55a0932514f4588d87",
-    "url": "/Zeldog/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "4fe5bc9e898ac2058359",
-    "url": "/Zeldog/static/css/main.b1e661f1.chunk.css"
+    "url": "/static/css/main.b1e661f1.chunk.css"
   },
   {
     "revision": "996eb34c7cb3b38fb238",
-    "url": "/Zeldog/static/js/2.6e84e58a.chunk.js"
+    "url": "/static/js/2.6e84e58a.chunk.js"
   },
   {
     "revision": "4fe5bc9e898ac2058359",
-    "url": "/Zeldog/static/js/main.a27655e2.chunk.js"
+    "url": "/static/js/main.a27655e2.chunk.js"
   },
   {
     "revision": "3cbe931bc7b8dc7063ab",
-    "url": "/Zeldog/static/js/runtime-main.dd286bee.js"
+    "url": "/static/js/runtime-main.dd286bee.js"
   },
   {
     "revision": "3d026e275b43480a0b101dcedf0ffde4",
-    "url": "/Zeldog/static/media/bg.3d026e27.png"
+    "url": "/static/media/bg.3d026e27.png"
   },
   {
     "revision": "90b7be82cd37eb98b01863f50b84ab62",
-    "url": "/Zeldog/static/media/bunny-hop-spritesheet.90b7be82.png"
+    "url": "/static/media/bunny-hop-spritesheet.90b7be82.png"
   },
   {
     "revision": "154a590f6941d4685321ee4c61d25385",
-    "url": "/Zeldog/static/media/coin.154a590f.gif"
+    "url": "/static/media/coin.154a590f.gif"
   },
   {
     "revision": "3cb9b2a95b2570a03a71211918d6a55c",
-    "url": "/Zeldog/static/media/coin.3cb9b2a9.mp3"
+    "url": "/static/media/coin.3cb9b2a9.mp3"
   },
   {
     "revision": "06f517f84a8b8ded09edeb45ba9052cd",
-    "url": "/Zeldog/static/media/game.06f517f8.mp3"
+    "url": "/static/media/game.06f517f8.mp3"
   },
   {
     "revision": "299ed4201c491621316239a7f9fe9621",
-    "url": "/Zeldog/static/media/gameOver.299ed420.mp3"
+    "url": "/static/media/gameOver.299ed420.mp3"
   },
   {
     "revision": "dfb0b609d5324e967c35280436a4f22b",
-    "url": "/Zeldog/static/media/heart.dfb0b609.png"
+    "url": "/static/media/heart.dfb0b609.png"
   },
   {
     "revision": "8bb2e74fada6cf5fbc3064db9722dccd",
-    "url": "/Zeldog/static/media/hurt.8bb2e74f.mp3"
+    "url": "/static/media/hurt.8bb2e74f.mp3"
   },
   {
     "revision": "7477a45b4828c4076de5288333876711",
-    "url": "/Zeldog/static/media/monster1.7477a45b.png"
+    "url": "/static/media/monster1.7477a45b.png"
   },
   {
     "revision": "fd72794d28ca5842d6767c31a4ad9d27",
-    "url": "/Zeldog/static/media/monster2.fd72794d.png"
+    "url": "/static/media/monster2.fd72794d.png"
   },
   {
     "revision": "70d953ef442c67012158a61115dd6984",
-    "url": "/Zeldog/static/media/monster3.70d953ef.png"
+    "url": "/static/media/monster3.70d953ef.png"
   },
   {
     "revision": "abd5fa7408f2057576a552ff6321755c",
-    "url": "/Zeldog/static/media/monster4.abd5fa74.png"
+    "url": "/static/media/monster4.abd5fa74.png"
   },
   {
     "revision": "3e46d1050d58acab22e0c301267e323e",
-    "url": "/Zeldog/static/media/player_walk.3e46d105.png"
+    "url": "/static/media/player_walk.3e46d105.png"
   },
   {
     "revision": "0a8de2641c207942499c8a572ca4ccdb",
-    "url": "/Zeldog/static/media/victory.0a8de264.gif"
+    "url": "/static/media/victory.0a8de264.gif"
   },
   {
     "revision": "12ae9b7572f0b3956fea0e9592903048",
-    "url": "/Zeldog/static/media/victory.12ae9b75.mp3"
+    "url": "/static/media/victory.12ae9b75.mp3"
   }
 ]);
